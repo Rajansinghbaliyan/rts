5f4f380d103531113c3fc33daae0dafd47fd8d49
RajanBaliyan <rajansinghbaliyan@gmail.com>
2022-10-26 09:19:22 +0530

    feat: usage of use state and callback functions

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,9 +1,21 @@
+import React from "react";
+
 interface ChildProps {
-    color: string
+    color: string,
+    onClick: () => void;
 }
-export const Child = ({color}: ChildProps) => {
+
+export const Child = ({color, onClick}: ChildProps) => {
     return <div>
         <h1>Hi from the child</h1>
         <span>color selected is {color}</span>
+        <button onClick={onClick}>Click Me!</button>
+    </div>;
+}
+export const ChildAsFc: React.FC<ChildProps> = ({color, onClick}) => {
+    return <div>
+        <h1>From the functional components</h1>
+        <span>color is {color}</span>
+        <button onClick={onClick}>Click Me!</button>
     </div>;
 }
--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -1,7 +1,24 @@
-import {Child} from "./Child";
+import {Child, ChildAsFc} from "./Child";
+import {useState} from "react";
 
 const Parent = () => {
-    return <Child color={'red'} />;
+
+    const [childTimes, setChildTimes] = useState(0);
+    const [childFcTimes, setChildFcTimes] = useState(0);
+
+    const onClickHandlerChild = () => {
+        console.log("Child is clicked" + childTimes);
+        setChildTimes(prevState => prevState + 1);
+    }
+
+    const onClickHandlerChildAsFc = () => {
+        console.log("ChildAsFc is clicked" + childFcTimes);
+        setChildFcTimes(prevState => prevState + 1);
+    }
+    return <>
+        <Child color={'red'} onClick={onClickHandlerChild} />
+        <ChildAsFc color={'black'} onClick={onClickHandlerChildAsFc}/>
+    </>
 }
 
 export default Parent;
